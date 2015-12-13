1d11da6fd1aa86bdd9a09b2b7a88f1124f371903
vlad <vlad@r930.com>
2015-12-13 23:56:36 +0000

    added some python functions

--- Python/functions.py
+++ Python/functions.py
@@ -51,3 +51,29 @@ def anti_vowel(text):
     for v in vowels:
             text = text.replace(v, '')
     return text
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+         
+def scrabble_score(word):
+    word = word.lower()
+    result = 0;
+    for l in word:
+        result += score[l]
+        
+    return result
+
+def censor(text, word):
+    text = text.replace(word, '*' * len(word))
+    return text
+
+def count(sequence, item):
+    c = 0
+    for i in sequence:
+        if i == item:
+            c += 1
+
+    return c
