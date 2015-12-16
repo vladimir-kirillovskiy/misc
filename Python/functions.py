e4d3525a14b215495fa9221b4970044ca32097cf
vladk <vlad@work.com>
2015-12-16 16:17:26 +0000

    python functions

--- Python/functions.py
+++ Python/functions.py
@@ -91,3 +91,11 @@ def product(intList):
     for i in intList:
         result *= i
     return result
+
+def remove_duplicates(lst):
+    new_list = [];
+    for i in lst:
+        if (i not in new_list):
+            new_list.append(i)
+            
+    return new_list
