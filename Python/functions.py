045513c069857134fdf00b000e15b7f3085d57ca
vlad <vlad@r930.com>
2018-11-05 21:32:43 +0300

    a bunch of diffirent notes

--- Python/functions.py
+++ Python/functions.py
@@ -137,3 +137,25 @@ def flip_bit(number, n):
     mask = mask << n - 1
     result = number ^ mask
     return bin(result)
+
+
+# scrap data example
+from bs4 import BeautifulSoup
+ 
+import requests
+import sys
+ 
+url = 'http://www.imdb.com/chart/top'
+response = requests.get(url)
+soup = BeautifulSoup(response.text)
+tr = soup.findChildren("tr")
+tr = iter(tr)
+next(tr)
+ 
+for movie in tr:
+title = movie.find('td', {'class': 'titleColumn'} ).find('a').contents[0]
+year = movie.find('td', {'class': 'titleColumn'} ).find('span', {'class': 'secondaryInfo'}).contents[0]
+rating = movie.find('td', {'class': 'ratingColumn imdbRating'} ).find('strong').contents[0]
+row = title + ' - ' + year + ' ' + ' ' + rating
+ 
+print(row)
